ea6d559a1e7679b23d00819f36f1cb6e7ac706ca
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-29 21:02:57 +0200

    basic tui-pagination

--- src/library.js
+++ src/library.js
@@ -1,16 +1,17 @@
 import Pagination from 'tui-pagination';
 
 const options = {
-    totalItems: 500,
+    totalItems: 200,
     template: {
         page: '<a href="#" class="main-section__page-number">{{page}}</a>',
         currentPage: '<a href="#" class="main-section__page-number main-section__page-number--active">{{page}}</a>',
-        moveButton: '<a class="main-section__arrows"><svg class="main-section__arrow-icon"><use href="./images/sprite.svg#icon-arrow-left"></use></svg></a>',
-        disabledMoveButton: '<a class="main-section__arrows"><svg class="main-section__arrow-icon"><use href="../images/sprite.svg#icon-arrow-left"></use></svg></a>',
+        moveButton: '<a class="main-section__arrows main-section__arrows--{{type}} "></a>',
+        disabledMoveButton: '<a style="display: none">',
     }
 }
 
 const paginationContainer = document.querySelector('.tui-pagination');
-const pagination = new Pagination(paginationContainer, options)
+const pagination = new Pagination(paginationContainer, options);
+
 
 pagination.getCurrentPage();
