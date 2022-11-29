0dfcaaf5b52042365bdbaff14dc350ce9c06de64
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-29 21:57:03 +0200

    modal window open/close
    + overflow + pointer-events none on visually hidden

---
+++ src/js/modal.js
@@ -0,0 +1,31 @@
+const cardDivs = document.querySelector('.main-section__allcards');
+const modal = document.querySelector('.modal');
+const overflow = document.querySelector('.overflow');
+const closeBtn = document.querySelector('.modal__close-btn')
+
+cardDivs.addEventListener('click', showModal);
+
+function showModal(e) {
+    if (e.currentTarget.nodeName === 'DIV') {
+        modal.classList.remove('visually-hidden');
+        overflow.classList.remove('visually-hidden');
+        document.addEventListener('keydown', closeModalonEsc);
+        closeBtn.addEventListener('click', closeModal);
+        overflow.addEventListener('click', closeModalOverflow);
+    }
+}
+
+function closeModalOverflow(e) {
+    if (e.currentTarget === e.target) closeModal();
+}
+
+function closeModalonEsc(e) {
+    if (e.code === 'Escape') closeModal();  
+}
+
+function closeModal() {
+    modal.classList.add('visually-hidden');
+    overflow.classList.add('visually-hidden');
+    document.removeEventListener('keydown', closeModal);
+    closeBtn.removeEventListener('click', closeModal);
+}
