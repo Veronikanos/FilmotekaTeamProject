7150d6c6618e822e0cdcd1012792990673a6b3b7
smerch88 <maxarmyk@gmail.com>
2022-12-01 14:22:11 +0200

    Buttons class add-remove JS

--- src/js/library-page-only/filter.js
+++ src/js/library-page-only/filter.js
@@ -9,10 +9,24 @@ const DATA_KEY2 = 'queue';
 
 function renderWatched(event) {
   event.preventDefault();
+
+  event.target.classList.remove('inactive');
+  event.target.classList.add('active');
+
+  queuedButRef.classList.remove('active');
+  queuedButRef.classList.add('inactive');
+
   renderData(DATA_KEY1);
 }
 function renderQueue(event) {
   event.preventDefault();
+
+  event.target.classList.remove('inactive');
+  event.target.classList.add('active');
+
+  watchedButRef.classList.remove('active');
+  watchedButRef.classList.add('inactive');
+
   renderData(DATA_KEY2);
 }
 
