7e307b738250a0ffbaf845af275229dfc0a2ae04
Veronica <vveronikanika@gmail.com>
2022-12-01 20:15:12 +0200

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   allCardsSection: document.querySelector('.main-section__allcards'),
   headerFormInput: document.querySelector('form.header__form'),
+  noResultsText: document.querySelector('.header__form-warning-text'),
 };
