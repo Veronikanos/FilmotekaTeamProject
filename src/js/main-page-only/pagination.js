7fb62f39a9a677af6d368e3e3a81d8bc5bb87acb
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-01 21:45:27 +0200

    created branch from library-modal

--- src/js/main-page-only/pagination.js
+++ src/js/main-page-only/pagination.js
@@ -1,27 +1,36 @@
 import Pagination from 'tui-pagination';
-import MoviesApiService from '../api-service';
+import { moviesApiService } from '../init';
 import { renderSearchResult } from '../render-markup';
 import { refs } from '../refs';
 
 const { paginationContainer, allCardsSection } = refs;
 
-const options = {
-  totalItems: 200,
-  template: {
-    page: '<a href="#" class="main-section__page-number">{{page}}</a>',
-    currentPage:
-      '<a href="#" class="main-section__page-number main-section__page-number--active">{{page}}</a>',
-    moveButton:
-      '<a class="main-section__arrows main-section__arrows--{{type}} "></a>',
-    disabledMoveButton: '<a style="display: none">',
-  },
-};
+export function createPagination(fetchType) {
+  const totalResults = localStorage.getItem('totalResults');
 
-const pagination = new Pagination(paginationContainer, options);
+  const options = {
+    totalItems: JSON.parse(totalResults),
+    itemsPerPage: 20,
+    template: {
+      page: '<a href="#" class="main-section__page-number">{{page}}</a>',
+      currentPage:
+        '<a href="#" class="main-section__page-number main-section__page-number--active">{{page}}</a>',
+      moveButton:
+        '<a class="main-section__arrows main-section__arrows--{{type}} "></a>',
+      disabledMoveButton: '<a style="display: none">',
+    },
+  };
 
-pagination.on('afterMove', async function (eventData) {
-  const paginatedMovieApiService = new MoviesApiService();
-  paginatedMovieApiService.page = eventData.page;
-  const movies = await paginatedMovieApiService.fetchTrending();
-  allCardsSection.innerHTML = renderSearchResult(movies.data.results).join('');
-});
+  const pagination = new Pagination(paginationContainer, options);
+  pagination.on('afterMove', async function (eventData) {
+    moviesApiService.page = eventData.page;
+    const movies =
+      fetchType === 'trending'
+        ? await moviesApiService.fetchTrending()
+        : await moviesApiService.fetchMoviesByKeyword();
+
+    allCardsSection.innerHTML = renderSearchResult(movies.data.results).join(
+      ''
+    );
+  });
+}
