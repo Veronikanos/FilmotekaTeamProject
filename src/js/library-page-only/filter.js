9d47e969ed75dcc0541b828a389b98118004c504
Veronica <vveronikanika@gmail.com>
2022-12-01 16:07:14 +0200

    refactor: remove inactive class, switch button status when LS is empty

--- src/js/library-page-only/filter.js
+++ src/js/library-page-only/filter.js
@@ -12,34 +12,25 @@ const DATA_KEY2 = 'queue';
 function renderWatched(event) {
   event.preventDefault();
 
-  let dataToRender = JSON.parse(localStorage.getItem(DATA_KEY1));
-  if (!dataToRender) {
+  event.target.classList.add('active');
+  queuedButRef.classList.remove('active');
+
+  if (!localStorage.getItem(DATA_KEY1)) {
     Notiflix.Notify.warning('No movies added!');
     return;
   }
-
-  event.target.classList.remove('inactive');
-  event.target.classList.add('active');
-
-  queuedButRef.classList.remove('active');
-  queuedButRef.classList.add('inactive');
-
   renderData(DATA_KEY1);
 }
 function renderQueue(event) {
   event.preventDefault();
 
-  let dataToRender = JSON.parse(localStorage.getItem(DATA_KEY2));
-  if (!dataToRender) {
+  event.target.classList.add('active');
+  watchedButRef.classList.remove('active');
+
+  if (!localStorage.getItem(DATA_KEY2)) {
     Notiflix.Notify.warning('No movies added!');
     return;
   }
-
-  event.target.classList.remove('inactive');
-  event.target.classList.add('active');
-
-  watchedButRef.classList.remove('active');
-  watchedButRef.classList.add('inactive');
 
   renderData(DATA_KEY2);
 }
